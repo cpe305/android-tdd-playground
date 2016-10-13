a2b5cbc16c5fb283ed835a20582a3c60b77ea1d0
gsocher <gudrun.socher@hm.edu>
2016-10-13 15:23:06 -0700

    put integration test back in

--- app/src/androidTest/java/org/pestrada/android_tdd_playground/ApplicationTest.java
+++ app/src/androidTest/java/org/pestrada/android_tdd_playground/ApplicationTest.java
@@ -7,10 +7,9 @@ import android.test.ApplicationTestCase;
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
-     /* if testing wanted, uncomment before_script in .travis.yml
+
     public ApplicationTest() {
-       
-       // super(Application.class);
+       super(Application.class);
     }
-    */
+    
 }
